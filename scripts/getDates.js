b8e1bc8e7d9b2fbd0fd2b845757ee0f68b50613f
Ethan Glenn <ethan+git@eglenn.dev>
2024-01-18 08:43:26 -0700

    Changed let to const for all variables

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Getting the year and inserting it into the footer
     const year = new Date().getFullYear();
-    let footerYear = document.getElementById('footer-year');
+    const footerYear = document.getElementById('footer-year');
     footerYear.textContent = year;
 
     // Getting the last modified date for the document and inserting it into the footer
     const docLastMod = new Date(document.lastModified);
-    let footerMod = document.getElementById('last-updated');
+    const footerMod = document.getElementById('last-updated');
     footerMod.textContent = `${docLastMod.toDateString()} ${docLastMod.getHours()}:${docLastMod.getMinutes()}:${docLastMod.getSeconds()}`;
     // footerMod.textContent = docLastMod;
 });
